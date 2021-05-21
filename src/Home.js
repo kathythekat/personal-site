4ba27fc84a263594c360971ddde1fed108ed6388
Kat Huang <khuang129@gmail.com>
2021-05-21 08:38:26 -0700

    pauses animation after mouse enter event

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import {useRef} from 'react';
 import ConnectIcons from "./ConnectIcons";
 import Montana from "./images/montana.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,9 +8,16 @@ import Resume from './resume/KatHuang_Resume.pdf'
 const stack = <FontAwesomeIcon icon={faLayerGroup} size="2x" />;
 
 function Home() {
+  const animateRef = useRef(null);
+
+  function pauseAnimation(e) {
+    animateRef.current.classList.remove('animate-wiggle')
+  }
+
   return (
     <div className="h-screen d-flex flex-col items-center justify-center">
-      <div className="lg:h-1/2 mt-10 mb-6 lg:my-20 flex flex-col lg:flex-row justify-center items-center">
+      <div 
+        className="lg:h-1/2 mt-10 mb-6 lg:my-20 flex flex-col lg:flex-row justify-center items-center">
         <img
           className="w-10/12 max-w-sm lg:max-w-md h-auto rounded-lg shadow-lg"
           alt="Montana"
@@ -24,9 +32,13 @@ function Home() {
             Python, Tailwind, Sass, Bootstrap, JQuery, Jest, Jasmine
           </h4>
           <a href={Resume} target="_blank" rel="noreferrer">
-        <button className="text-green-500 bg-transparent border border-solid border-green-500 hover:bg-green-400 hover:text-white active:bg-blue-600 font-bold text-md mt-4 px-6 py-3 rounded-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-500 animate-wiggle" type="button"
+        <button 
+        ref={animateRef}
+        className="text-green-500 bg-transparent border border-solid border-green-500 hover:bg-green-400 hover:text-white active:bg-blue-600 font-bold text-md mt-4 px-6 py-3 rounded-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-500 animate-wiggle" 
+        type="button"
+        onMouseEnter={pauseAnimation}
         >
-         Want me on your team? <i class="fas fa-laptop"></i>
+         Want me on your team? <i className="fas fa-laptop"></i>
         </button>
         </a>
         </div>

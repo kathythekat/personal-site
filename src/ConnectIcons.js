fddeb666915a5d2274a09288ab76e2443734567f
Kat Huang <khuang129@gmail.com>
2021-06-13 19:17:38 -0700

    added twitter icon

--- src/ConnectIcons.js
+++ src/ConnectIcons.js
@@ -4,14 +4,16 @@ import {
   faInstagram,
   faLinkedin,
   faStrava,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const githubIcon = <FontAwesomeIcon icon={faGithub} size="2x" />;
 const instagramIcon = <FontAwesomeIcon icon={faInstagram} size="2x" />;
 const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} size="2x" />;
 const stravaIcon = <FontAwesomeIcon icon={faStrava} size="2x" />;
 const emailIcon = <FontAwesomeIcon icon={faEnvelope} size="2x" />;
+const twitterIcon = <FontAwesomeIcon icon={faTwitter} size="2x" />;
 
 const icons = [
   { icon: emailIcon, url: "mailto:khuang129@gmail.com" },
@@ -22,12 +24,13 @@ const icons = [
     url: "https://www.linkedin.com/in/kat-huang-599b6a66/",
   },
   { icon: stravaIcon, url: "https://www.strava.com/athletes/46730453" },
+  { icon: twitterIcon, url: "https://twitter.com/kathyiskat" },
 ];
 
 function ConnectIcons() {
   return (
     <div className="flex flex-row justify-center items-center lg:text-lg">
-      <p className="hidden lg:block">Connect with me:{" "}</p>
+      <p className="hidden lg:block">Connect with me: </p>
       {icons.map((i, idx) => (
         <div className="mx-2" key={idx}>
           <a className="fill-current hover:text-purple-500 " href={i.url}>
--- src/Home.js
+++ src/Home.js
@@ -33,7 +33,7 @@ function Home() {
           <a href={Resume} target="_blank" rel="noreferrer" className="w-40">
             <button
               ref={animateRef}
-              className="text-white bg-seagreen hover:bg-forest hover:text-white active:bg-green-500 text-sm mt-2 mb-4 px-4 py-3 rounded-lg outline-none focus:outline-none mr-2 items-center w-52"
+              className="text-white bg-seagreen hover:bg-forest hover:text-white active:bg-green-500 text-sm mt-2 mb-4 p-3 rounded-lg outline-none focus:outline-none mr-2 items-center w-52"
               type="button"
               onMouseEnter={wiggleButton}
               onMouseLeave={stopWiggle}
